7b246133579a00fffde36988298150285522576a
BeyondNaxos <adopteuntrip@gmail.com>
2023-04-11 01:56:26 +0200

    Indent

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import {Routes , Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from '../Pages/Home/Home'
 import List from '../Pages/List/List'
 
 const Routing = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/list" element={<List/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
     </Routes>
   )
-  
 }
 
 export default Routing
