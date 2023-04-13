3a9f98ce6636a7333034131bd6491f27b32a17d9
BeyondNaxos <adopteuntrip@gmail.com>
2023-04-13 10:54:05 +0200

    Adding Doc

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,14 @@ import Footer from './Components/Footer/Footer.jsx'
 import { createContext, useState } from 'react'
 import * as mockData from './mock/mockedData.json'
 
+/* This code is creating a React context called `usersInfosContext` using the `createContext()`
+function. It is then defining a component called `App` which is a parent component that wraps around
+other components. Within the `App` component, it is using the `useState()` hook to create a state
+variable called `usersInfos` and a function called `setUsersInfos` to update the state. It is then
+passing these values as a context value to all child components using the
+`usersInfosContext.Provider` component. Finally, it is exporting the `usersInfosContext` and the
+`App` component as the default exports of the module. */
+
 const usersInfosContext = createContext()
 
 function App() {
--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -2,6 +2,15 @@ import React, { useEffect } from 'react'
 import styles from './Modal.module.css'
 import { LinearProgress } from '@mui/material'
 
+/* This is a React component called `Modal`. It displays a message "L'employé a bien été créé" (which
+means "The employee has been created successfully" in French) and a progress bar. The progress bar
+starts at 0 and increases to 100% over a period of 3 seconds (3000ms). The `wait` state is used to
+control whether the progress bar should be displayed or not. When `wait` is true, the progress bar
+is displayed, and when it becomes false after 3 seconds, the progress bar disappears. The
+`useEffect` hook is used to update the progress bar every 30ms. The `LinearProgress` component is
+imported from the Material-UI library and used to display the progress bar. The CSS styles for the
+component are defined in a separate file called `Modal.module.css`. */
+
 const Modal = () => {
 
     const [wait , setWait] = React.useState(true)
