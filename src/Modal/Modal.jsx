5629c562ff10062d439b844995da00e2792c1cf3
BeyondNaxos <adopteuntrip@gmail.com>
2023-05-08 13:24:42 +0200

    Update Package

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -12,39 +12,35 @@ imported from the Material-UI library and used to display the progress bar. The 
 component are defined in a separate file called `Modal.module.css`. */
 
 const Modal = () => {
+  const [wait, setWait] = React.useState(true)
 
-    const [wait , setWait] = React.useState(true)
+  const [progress, setProgress] = React.useState(0)
 
-    const [progress, setProgress] = React.useState(0)
+  // from 0 to 3000 ms
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 0 : prevProgress + 10
+      )
+    }, 30)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
 
-    // from 0 to 3000 ms
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10))
-        }, 30)
-        return () => {
-            clearInterval(timer)
-        }
-    }, [])
-
-    setTimeout(() => {
-        setWait(false)
-    }, 3000)
-
-    
-
+  setTimeout(() => {
+    setWait(false)
+  }, 3000)
 
   return (
     <div className={styles.modalContainer}>
-        <p className={styles.modal}>L'employé a bien été créé</p>
-        {/* progress bar */}
-        {
-            wait &&
-     <div style={{ width: 100 }}>
-        <LinearProgress value={progress} />
-     </div>
-            
-        }
+      <p className={styles.modal}>L'employé a bien été créé</p>
+      {/* progress bar */}
+      {wait && (
+        <div style={{ width: 100 }}>
+          <LinearProgress value={progress} />
+        </div>
+      )}
     </div>
   )
 }
