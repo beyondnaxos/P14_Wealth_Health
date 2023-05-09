459f062d23083cb525ace9490a0776280668eeda
BeyondNaxos <adopteuntrip@gmail.com>
2023-05-09 09:32:59 +0200

    table bg changed

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -33,7 +33,7 @@ const List = () => {
       <Datablify
         data={usersInfos}
         categories={categories}
-        headColor={'green'}
+        headColor={'white'}
         titleHeadColor={'black'}
       />
     </div>
